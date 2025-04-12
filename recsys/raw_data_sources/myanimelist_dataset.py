1ffd0d1b720825d18b36a67e37999ceec069370d
Shanilka Haturusinghe <s.haturusinghe99@gmail.com>
2025-04-12 18:15:28 +0530

    add function to extract anime data from MyAnimeList dataset

---
+++ recsys/raw_data_sources/myanimelist_dataset.py
@@ -0,0 +1,25 @@
+import polars as pl
+from pathlib import Path
+
+def extract_anime_data() -> pl.DataFrame:
+    """
+    Extracts anime data from the MyAnimeList dataset.
+
+    Returns:
+        pl.DataFrame: A DataFrame containing the anime data.
+    """
+    # __file__ is the current file, so go two directories up to the project root.
+    base_dir = Path(__file__).resolve().parent.parent.parent
+    csv_path = base_dir / "kaggle" / "anime.csv"
+    synopsis_csv_path = base_dir / "kaggle" / "anime_with_synopsis.csv"
+
+    df = pl.read_csv(csv_path, null_values=["Unknown"])
+    df_synopsis = pl.read_csv(synopsis_csv_path, null_values=["Unknown"])
+    joined_df = df.join(df_synopsis, on="MAL_ID", suffix="_dupe")
+
+    column_names: list[str] = joined_df.columns
+
+    columns_to_drop = [name for name in column_names if "_dupe" in name]
+    joined_df = joined_df.drop(columns_to_drop)
+
+    return joined_df
