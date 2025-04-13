e37f387563d16282bddde7b6b270967c1a16b580
Shanilka Haturusinghe <s.haturusinghe99@gmail.com>
2025-04-13 09:44:06 +0530

    fix: enhance docstring and improve sorting logic in compute_features_of_user function

--- recsys/features/user.py
+++ recsys/features/user.py
@@ -5,7 +5,10 @@ def compute_features_of_user(df: pl.DataFrame) -> pl.DataFrame:
     Prepare user data by performing several data cleaning and transformation steps.
 
     This function does the following:
-    1. ...
+    1. Checks for required columns in the input DataFrame
+    2. Sorts data by user_id and by rating (descending)
+    3. Groups data by user_id
+    4. For each user, aggregates the top 20 anime IDs and their ratings
 
     Parameters:
     - df (pl.DataFrame): Input DataFrame containing user rating data.
@@ -24,6 +27,9 @@ def compute_features_of_user(df: pl.DataFrame) -> pl.DataFrame:
     if missing_columns:
         raise ValueError(f"Missing {','.join(missing_columns)} columns in dataframe")
     
-    df = df.sort('user_id',descending=False).sort('rating').group_by('user_id').agg(pl.col("anime_id").alias("top_anime"), pl.col('rating').alias("top_ratings"))
+    df = df.sort('user_id').sort('rating', descending=True).group_by('user_id').agg(
+        pl.col("anime_id").list.head(20).alias("top_anime"), 
+        pl.col('rating').list.head(20).alias("top_ratings")
+    )
 
     return df
